75977891a0961d3627d2ca707dc2c167d6587896
joker <824219521@qq.com>
2016-01-15 09:47:13 +0800

    add a script for ubuntu 15.10 linux distribution

---
+++ ubuntu15.10_conf.sh
@@ -0,0 +1,14 @@
+# !/usr/bin/bash
+
+#Install ibus-Wubi input method
+#sudo apt-get install ibus-table-wubi
+
+#Install fcitx-wbpy input method
+sudo apt-get install fcitx-table-wbpy
+
+#Restart OS to allow input method take effect 
+#When input method been installe, the os should be reboot
+#reboot
+
+#Install git version control tools
+sudo apt-get install git
