05e6d61efd42e3f01f5d46c4c4c2990fa2fe218a
wangzhizhou <824219521@qq.com>
2019-08-01 17:00:15 +0800

    add ubuntu nginx https suport

---
+++ https/certbot_nginx_ubuntu18.04.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot python-certbot-nginx
+
+# automation setup https for nginx
+sudo certbot --nginx
+# manual setup https for nginx
+#sudo certbot certonly --nginx
+
+# Test automatic renewal
+#sudo certbot renew --dry-run
