a54de36b448817e5ae93a6d404d52895a11cb4e6
wangzhizhou <wangzhizhou@bytedance.com>
2021-02-13 00:12:52 +0800

    python3-cert-bot

--- https/certbot_nginx_ubuntu18.04.sh
+++ https/certbot_nginx_ubuntu18.04.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y universe
 sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install -y certbot python-certbot-nginx
+sudo apt-get install -y certbot python3-certbot-nginx
 
 # automation setup https for nginx
 sudo certbot --nginx
